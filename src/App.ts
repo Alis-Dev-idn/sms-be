733939ffe71338fbf814102834490fb1ea15abab
ibnu.badar <ibnu.badar@iconpln.co.id>
2025-05-09 11:41:04 +0700

    move config to SmsApplication.ts

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,3 @@
-import {config} from "dotenv";
-config();
-
 import SmsApplication from "./main/SmsApplication";
+
 SmsApplication.start();
--- src/main/SmsApplication.ts
+++ src/main/SmsApplication.ts
@@ -1,3 +1,6 @@
+import {config} from "dotenv";
+config();
+
 import cors from "cors";
 import http from "http";
 import morgan from "morgan";
