6dc27ef035c94ff2555fb9466f285f921b3b43c9
Alis-Dev-idn <ali.dev313@gmail.com>
2025-05-13 09:31:02 +0700

    update route user

--- src/main/user/User.ts
+++ src/main/user/User.ts
@@ -1,11 +1,9 @@
 import {Router} from "express";
+import UserController from "./UserController";
 
 const router = Router();
 
 export default (): Router => {
-    router.get("/", (req, res) => {
-        res.send("User route ok");
-    })
-
+    router.get("/", UserController.get("read"));
     return router;
 }
