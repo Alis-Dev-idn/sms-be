b2cd8f5023b29669367152dece7639d0d0b1072e
Alis-Dev-idn <ali.dev313@gmail.com>
2025-05-13 09:30:18 +0700

    update route

--- src/main/auth/Auth.ts
+++ src/main/auth/Auth.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
+import AuthController from "./AuthController";
 
 const router = Router();
 
 export default (): Router => {
-    router.get("/", (req, res) => {
-        res.send("Auth route ok");
-    })
+    router.post("/", AuthController.login());
+    router.post("/logout", AuthController.logout());
+    router.post("/register", AuthController.register());
 
     return router;
 }
