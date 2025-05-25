17c20d987f6373dfc4657f343d59237cae0db3fc
Alis-Dev-idn <ali.dev313@gmail.com>
2025-05-25 22:46:39 +0700

    add ci

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const fs = require("fs-extra");
 const commands = [
     "rimraf ./dist && rimraf ./buildApp",
     "npx tsc --build",
-    "pkg --compress GZip --output ./buildApp/pemetaan-api package.json",
+    "pkg --compress GZip --output ./buildApp/sms-be package.json",
     "rimraf ./dist"
 ];
 
